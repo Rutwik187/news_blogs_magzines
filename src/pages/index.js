2e7c1063201fb73749581af4bbc4c0b40fb5ae3f
Rutwik Shinde <rutwikshinde34@gmail.com>
2024-03-03 11:29:54 +0530

    made few build changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,18 @@
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-import { getAllPosts } from "../../lib/api";
 import HeadMeta from "../components/elements/HeadMeta";
 import FooterOne from "../components/footer/FooterOne";
-import FooterTwo from "../components/footer/FooterTwo";
 import HeaderOne from "../components/header/HeaderOne";
 import PostSectionFive from "../components/post/PostSectionFive";
 import PostSectionFour from "../components/post/PostSectionFour";
-import PostSectionOne from "../components/post/PostSectionOne";
 import PostSectionSix from "../components/post/PostSectionSix";
 import PostSectionThree from "../components/post/PostSectionThree";
 import PostSectionTwo from "../components/post/PostSectionTwo";
+import PostSectionOne from "../components/post/PostSectionOne";
 
 import SliderTwo from '../components/slider/SliderTwo';
 
-const HomeOne = ({allPosts}) => {
+const Home = () => {
  
   return ( 
     <div suppressHydrationWarning>
@@ -27,36 +25,12 @@ const HomeOne = ({allPosts}) => {
     <PostSectionFour />
     <PostSectionFive  />
     <PostSectionSix/>
-    {/* <FooterOne /> */}
-    <FooterTwo/>
-    <ReactQueryDevtools initialIsOpen />
+    <FooterOne />
+    {/* <ReactQueryDevtools/> */}
    </div>
    );
 }
  
-export default HomeOne;
+export default Home;
 
-
-export async function getStaticProps() {
-  const allPosts = getAllPosts([
-    'postFormat',
-    'trending',
-    'story',
-    'slug',
-    'title',
-    'excerpt',
-    'featureImg',
-    'cate',
-    'cate_bg',
-    'cate_img',
-    'author_name',
-    'date',
-    'post_views',
-    'post_share',
-  ])
-  
-  return {
-    props: { allPosts }
-  }
-}
 
