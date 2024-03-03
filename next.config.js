c5cbe556e747cfc3acb86b1e9eea792fa44a4338
Rutwik Shinde <rutwikshinde34@gmail.com>
2024-03-03 13:26:16 +0530

    removed the base path to configure deployment

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
-  basePath: process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASEPATH : "",
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
     return config;
