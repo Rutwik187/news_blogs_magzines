4b9e294766459290040f8c67dceb6f0bd42decc0
Rutwik Shinde <rutwikshinde34@gmail.com>
2024-04-20 21:36:31 +0530

    fixing build issues :

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "../styles/style.css";
-import "../styles/Magazines.css";
+import "../styles/magazines.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const queryClient = new QueryClient(); // Create a query client instance
