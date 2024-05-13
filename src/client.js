aaae01a23abd18f8a1a686991f88a8467f52bedd
Rutwik Shinde <rutwikshinde34@gmail.com>
2024-05-13 19:40:18 +0530

    moved to previos version of next and sanity/client

--- src/client.js
+++ src/client.js
@@ -1,11 +1,11 @@
-import { createClient } from "next-sanity";
+import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 // const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID // "pv8y60vp"
 // const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET // "production"
 // const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-05-03'
 
-export const client = createClient({
+export const client = sanityClient({
   projectId: "gaa41xdq",
   dataset: "production",
   apiVersion: "2023-05-03",
