c1c3c01a0d59cc91ffb0944a7e655ed8a24da9c5
Rutwik Shinde <rutwikshinde34@gmail.com>
2024-05-13 22:22:04 +0530

    moving posts form server to client component

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
+  webpack: (config, { isServer }) => {
+    // Prevent `fs` module not found error during build for server-side only packages
+    if (!isServer) {
+      config.resolve.fallback = { fs: false };
+    }
     return config;
   },
-   images: {
-    domains: ['cdn.sanity.io'],
+  images: {
+    domains: ["cdn.sanity.io"],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/post/[slug].js
+++ src/pages/post/[slug].js
@@ -1,26 +1,19 @@
+"use client";
 import HeadMeta from "../../components/elements/HeadMeta";
-
 import HeaderOne from "../../components/header/HeaderOne";
 import PostFormatText from "../../components/post/post-format/PostFormatText";
 import Magazines from "../../components/post/Magazines";
 import { client } from "../../client";
 import FooterTwo from "../../components/footer/FooterTwo";
+import { useRouter } from "next/router";
+import Loader from "../../components/common/Loader";
+import { useQuery } from "@tanstack/react-query";
 
-const PostDetails = ({ postContent }) => {
-  return (
-    <>
-      <HeadMeta metaTitle="Post Details" />
-      <HeaderOne />
-      <PostFormatText postData={postContent} />
-      <Magazines />
-      <FooterTwo />
-    </>
-  );
-};
+const PostDetails = () => {
+  const router = useRouter();
+  const { slug } = router.query;
 
-export const getServerSideProps = async ({ params }) => {
-  const { slug } = params;
-
+  console.log(slug);
   const postQuery = `*[_type == "post" && slug.current == '${slug}'][0] {
     title,
     slug,
@@ -28,13 +21,29 @@ export const getServerSideProps = async ({ params }) => {
     body
   }`;
 
-  const postContent = await client.fetch(postQuery);
-
-  return {
-    props: {
-      postContent,
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["magazines"],
+    queryFn: async () => {
+      const response = await client.fetch(postQuery);
+      return response;
     },
-  };
+  });
+
+  if (isLoading) return <Loader />;
+  if (error) return <div>Error fetching posts</div>;
+
+  if (!data) return null;
+
+  console.log(data);
+  return (
+    <>
+      <HeadMeta metaTitle="Post Details" />
+      <HeaderOne />
+      <PostFormatText postData={data} />
+      <Magazines />
+      {/* <FooterTwo /> */}
+    </>
+  );
 };
 
 export default PostDetails;
