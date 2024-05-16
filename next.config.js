412e5d647592221953c6e6fcb48977086756c79c
Rutwik Shinde <rutwikshinde34@gmail.com>
2024-05-16 13:17:15 +0530

    added staticprops again to posts made changes to next.config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 const nextConfig = {
   reactStrictMode: false,
-  swcMinify: true,
-  webpack: (config, { isServer }) => {
-    // Prevent `fs` module not found error during build for server-side only packages
-    if (!isServer) {
-      config.resolve.fallback = { fs: false };
-    }
-    return config;
-  },
+
   images: {
     domains: ["cdn.sanity.io"],
   },
