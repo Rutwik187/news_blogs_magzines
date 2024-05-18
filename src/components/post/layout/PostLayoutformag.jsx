1fac85fb72631be30000e4697203b3955b1e0b4d
Sahil-Rathod <sahilrathod2810@gmail.com>
2024-05-18 09:08:30 +0530

    added the slider

--- src/components/post/layout/PostLayoutformag.jsx
+++ src/components/post/layout/PostLayoutformag.jsx
@@ -3,64 +3,66 @@ import Link from "next/link";
 
 const PostLayoutformag = ({ data }) => {
   return (
-    <div className="post-container">
-      <Link href={`/magazine/${data.slug.current}`}>
-        <div className="image-container">
-          <Image
-            src={data.featureImg}
-            alt={data.title}
-            width={1000}
-            height={1000}
-            className="img-fluid"
-          />
+    <>
+      <div className="post-container">
+        <Link href={`/magazine/${data.slug.current}`}>
+          <div className="image-container">
+            <Image
+              src={data.featureImg}
+              alt={data.title}
+              width={1000}
+              height={1000}
+              className="img-fluid"
+            />
+          </div>
+        </Link>
+        <div className="title-container">
+          <div className="line" />
+          <h4
+            className="hover-line hover-line"
+            style={{ marginTop: "2rem", fontSize: "1.3rem", color: "#101820" }}
+          >
+            <Link href={`/magazine/${data.slug.current}`}>{data.title}</Link>
+          </h4>
         </div>
-      </Link>
-      <div className="title-container">
-        <div className="line" />
-        <h4
-          className="hover-line hover-line"
-          style={{ marginTop: "2rem", fontSize: "1.3rem", color: "#101820" }}
-        >
-          <Link href={`/magazine/${data.slug.current}`}>{data.title}</Link>
-        </h4>
+        <style jsx>{`
+          .post-container {
+            position: relative;
+            width: 100%;
+            margin-top: 20px; /* Adjust margin as needed */
+          }
+
+          .image-container {
+            transition: transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
+            box-shadow: #ae8625 2px 2px 5px 5px;
+          }
+
+          .image-container:hover {
+            transform: scale(1.1); /* Adjust the scale factor as needed */
+            box-shadow: #ae8625 0px 0px 8px 0px;
+          }
+
+          .title-container {
+            margin-top: 10px; /* Adjust margin as needed */
+            text-align: center;
+            position: relative;
+          }
+
+          .line {
+            width: 18rem;
+            height: 4px;
+            background-color: #101820;
+            margin: 3rem auto; /* Adjust margin as needed */
+          }
+
+          .title {
+            font-size: 1.5rem;
+            font-weight: bold;
+            text-align: center;
+          }
+        `}</style>
       </div>
-      <style jsx>{`
-        .post-container {
-          position: relative;
-          width: 100%;
-          margin-top: 20px; /* Adjust margin as needed */
-        }
-
-        .image-container {
-          transition: transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
-          box-shadow: #ae8625 2px 2px 5px 5px;
-        }
-
-        .image-container:hover {
-          transform: scale(1.1); /* Adjust the scale factor as needed */
-          box-shadow: #ae8625 0px 0px 8px 0px;
-        }
-
-        .title-container {
-          margin-top: 10px; /* Adjust margin as needed */
-          text-align: center;
-          position: relative;
-        }
-
-        .line {
-          width: 18rem;
-          height: 4px;
-          background-color: #101820;
-          margin: 3rem auto; /* Adjust margin as needed */
-        }
-
-        .title {
-          font-size: 1.5rem;
-          font-weight: bold;
-          text-align: center;
-        }
-      `}</style>
-    </div>
+    </>
   );
 };
 
---
+++ src/components/post/layout/PostLayoutformag_Home.jsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const PostLayoutformag_Home = ({ data }) => {
+  return (
+    <div className="content-block">
+      <Link href={`/magazine/${data.slug.current}`}>
+        <div
+          style={{
+            padding: "10px", // adjust padding as needed
+            border: "3px solid #ccc", // adjust the border style and color
+            borderRadius: "8px", // adjust border radius for rounded corners
+            boxShadow: "0 0 5px rgba(0, 0, 0, 0.1)", // add a subtle shadow
+            overflow: "hidden",
+            margin: "2rem",
+          }}
+        >
+          <div style={{ marginBottom: "10px" }}>
+            {" "}
+            {/* adjust spacing between image and title */}
+            <Image
+              src={data.featureImg}
+              alt={data.title}
+              width={1000}
+              height={1000}
+              className="img-fluid"
+              style={{ display: "block" }}
+            />
+          </div>
+          <div className="caption-content">
+            <h4
+              className="hover-line hover-line"
+              style={{
+                fontSize: "1.3rem",
+                color: "#101820",
+                textDecoration: "none",
+                marginTop: "2rem",
+                padding: "0",
+              }}
+            >
+              <Link
+                href={`/magazine/${data.slug.current}`}
+                style={{ color: "#101820", textDecoration: "none" }}
+              >
+                {data.title}
+              </Link>
+            </h4>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default PostLayoutformag_Home;
