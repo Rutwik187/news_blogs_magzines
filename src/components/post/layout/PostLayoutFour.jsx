b46e3037c2b77ffbcf87a03fd919c7b331cb6002
Sahil-Rathod <sahilrathod2810@gmail.com>
2024-05-18 08:01:45 +0530

    added the line and border shadow

--- src/components/post/layout/PostLayoutFour.jsx
+++ src/components/post/layout/PostLayoutFour.jsx
@@ -5,22 +5,48 @@ const PostLayoutFour = ({ data }) => {
   return (
     <div className="content-block m-b-xs-30 col-6 col-md-3">
       <Link href={`/magazine/${data.slug.current}`}>
-        <Image
-          src={data.featureImg}
-          alt={data.title}
-          width={1000}
-          height={1000}
-          className="img-fluid"
-        />
-        {/* <div className="grad-overlay" /> */}
-      </Link>
-      <div className="media-caption">
-        <div className="caption-content">
-          <h3 className="axil-post-title hover-line hover-line">
-            <Link href={`/magazine/${data.slug.current}`}>{data.title}</Link>
-          </h3>
+        <div
+          style={{
+            padding: "10px", // adjust padding as needed
+            border: "3px solid #ccc", // adjust the border style and color
+            borderRadius: "8px", // adjust border radius for rounded corners
+            boxShadow: "0 0 5px rgba(0, 0, 0, 0.1)", // add a subtle shadow
+            overflow: "hidden",
+          }}
+        >
+          <div style={{ marginBottom: "10px" }}>
+            {" "}
+            {/* adjust spacing between image and title */}
+            <Image
+              src={data.featureImg}
+              alt={data.title}
+              width={1000}
+              height={1000}
+              className="img-fluid"
+              style={{ display: "block" }}
+            />
+          </div>
+          <div className="caption-content">
+            <h4
+              className="hover-line hover-line"
+              style={{
+                fontSize: "1.3rem",
+                color: "#101820",
+                textDecoration: "none",
+                marginTop: "2rem",
+                padding: "0",
+              }}
+            >
+              <Link
+                href={`/magazine/${data.slug.current}`}
+                style={{ color: "#101820", textDecoration: "none" }}
+              >
+                {data.title}
+              </Link>
+            </h4>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
