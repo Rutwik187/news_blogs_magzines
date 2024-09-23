cc6a71e6ee1ac6bdc58e482c788797eb96b51434
Sahil-Rathod <sahilrathod2810@gmail.com>
2024-09-23 10:22:53 +0530

    added the logo slider

---
+++ sanity_backend/schemaTypes/brandlogo.js
@@ -0,0 +1,22 @@
+export default {
+  name: 'brand',
+  title: 'Brands Logo',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      validation: (Rule) => Rule.required(),
+    },
+  ],
+}
